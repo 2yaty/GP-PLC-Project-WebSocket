053dc98b5d11bed6f25765856973ec9d02d305eb
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2024-03-09 20:33:47 +0200

    initial commit

---
+++ src/main/java/com/plc/plc/PlcTestApplication.java
@@ -0,0 +1,13 @@
+package com.plc.plc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlcTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plc/plc/PlcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plc.plc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
